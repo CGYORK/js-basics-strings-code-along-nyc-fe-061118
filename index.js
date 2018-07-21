05fd00a572aba89f219305cafbef86ca9563e705
Christian Georgeou <cgeorgeou@hotmail.com>
2018-07-21 22:29:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 let currentUser = 'AUGGIE';
 const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + "!";
 const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser + "!";
-const firstInitial = currentUser(0);
+const firstInitial = currentUser[0];
 let shortGreeting = 'Welcome, ' + firstInitial + '!';
