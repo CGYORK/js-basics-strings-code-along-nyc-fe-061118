7817553187be962bf944b050a6130c958f44b5d2
Christian Georgeou <cgeorgeou@hotmail.com>
2018-07-21 22:29:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+let currentUser = 'AUGGIE';
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + "!";
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser + "!";
+const firstInitial = currentUser[0];
+let shortGreeting = 'Welcome, ' + firstInitial + '!';
